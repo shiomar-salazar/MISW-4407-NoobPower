37a0432ddcda72eb222af8cbf6f0823752ff6de9
SaragRam <sg.ramirez940@uniandes.edu.co>
2024-05-11 22:39:33 -0500

    update: ajustes y correcciones documento

--- src/ecs/systems/s_starfield.py
+++ src/ecs/systems/s_starfield.py
@@ -4,33 +4,31 @@ import pygame
 import esper
 from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
-from src.ecs.components.c_velocity import CVelocity
 from src.ecs.components.tags.c_tag_star import CTagStar
 
 
-def system_starfield(world:esper.World, delta_time:float, stars_info: dict, window_info: dict):
+def system_starfield(world: esper.World, delta_time: float, stars_info: dict, window_info: dict):
     components = world.get_components(CSurface, CTransform, CTagStar)
     star_colors = stars_info["star_colors"]
     blink_rate = stars_info["blink_rate"]
-    number_of_stars = stars_info["number_of_stars"]
-
-    stars_on_screen = 0
 
     for _, (c_s, c_t, _) in components:
-        if stars_on_screen >= number_of_stars:
-            break
-
         c_t.pos.y += random.uniform(stars_info["vertical_speed"]["min"], stars_info["vertical_speed"]["max"]) * delta_time
 
         if c_t.pos.y > window_info["size"]["h"]:
-            c_t.pos.y = random.randint(0, window_info["size"]["h"])
+            c_t.pos.y = 0
             c_t.pos.x = random.randint(0, window_info["size"]["w"])
-
-        color = random.choice(star_colors)
-        pygame.draw.circle(c_s.surf, pygame.Color(color["r"], color["g"], color["b"]), (0, 0), 1)
+            color = random.choice(star_colors)
+            c_s.color = color 
+            c_s.surf.fill(pygame.Color(color["r"], color["g"], color["b"]))
 
         if random.random() < blink_rate["min"]:
-            pygame.draw.circle(c_s.surf, pygame.Color(window_info["bg_color"]["r"], window_info["bg_color"]["g"], window_info["bg_color"]["b"]), (0, 0), 1)
+            c_s.surf.set_alpha(0)
+        elif random.random() > blink_rate["max"]:
+            c_s.surf.set_alpha(255)
+        else:
 
-        stars_on_screen += 1
+            alpha = 0 if random.random() < 0.5 else 255
+            c_s.surf.set_alpha(alpha)
+
 
