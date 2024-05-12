47150d21d7fdf8765c26434f2d7ac6311c8abcad
SaragRam <sg.ramirez940@uniandes.edu.co>
2024-05-11 22:46:01 -0500

    update: actualización play scene

--- src/game/play_scene.py
+++ src/game/play_scene.py
@@ -19,6 +19,7 @@ from src.ecs.systems.s_enemy_fire import system_enemy_fire
 from src.ecs.systems.s_explosion_kill import system_explosion_kill
 from src.ecs.systems.s_movement import system_movement
 from src.ecs.systems.s_player_limits import system_player_limits
+from src.ecs.systems.s_score import system_score
 from src.ecs.systems.s_screen_bullet import system_screen_bullet
 from src.engine.scenes.scene import Scene
 from src.engine.service_locator import ServiceLocator
@@ -39,16 +40,26 @@ class PlayScene(Scene):
         self.pause_text = None
 
     def do_create(self):
+        self._interface_cfg = ServiceLocator.configurations_service.get("assets/cfg/menu_screen.json")
         self.player_entity = create_player(self.ecs_world, self.player_cfg, self._screen_surf)
         self.player_c_v = self.ecs_world.component_for_entity(self.player_entity, CVelocity)
         self.player_c_t = self.ecs_world.component_for_entity(self.player_entity, CTransform)
         self.player_c_s = self.ecs_world.component_for_entity(self.player_entity, CSurface)
+        create_text(self.ecs_world, self._interface_cfg["00"]["text"], 
+                    self._interface_cfg["start"]["size"],  
+                    pygame.Color(self._interface_cfg["00"]["color"]["r"], 
+                                 self._interface_cfg["00"]["color"]["g"], 
+                                 self._interface_cfg["00"]["color"]["b"]), 
+                    pygame.Vector2(self._interface_cfg["00"]["position"]["x"], 
+                                    self._interface_cfg["00"]["position"]["y"]), 
+                    TextAlignment.CENTER)
         create_level(self.ecs_world, self.enemies_cfg, self.level_cfg, self.window_cfg)
         create_input_player(self.ecs_world)
 
     def do_update(self, delta_time: float): 
         if not self.is_paused:
             system_movement(self.ecs_world, delta_time)
+            system_score(self.ecs_world)
             system_enemy_block_movement(self.ecs_world, delta_time, self.window_cfg["size"]["w"])
             system_enemy_fire(self.ecs_world, delta_time, self.bullet_cfg)
             system_screen_bullet(self.ecs_world, self._screen_surf)
